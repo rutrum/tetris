887651f4061838458930885660d9f60754045952
David Purdum <purdum41@gmail.com>
2018-12-19 16:18:34 -0500

    Update with modern colors

--- colors.js
+++ colors.js
@@ -1,12 +1,12 @@
 function getColor(i) {
     switch (i) {
         case 0: return color(0,0,0);
-        case 1: return color(15,100,50);
-        case 2: return color(15,250,100);
-        case 3: return color(55,100,200);
-        case 4: return color(150,30,50);
-        case 5: return color(150,0,0);
+        case 1: return color(255, 51, 153);
+        case 2: return color(255, 153, 0);
+        case 3: return color(255, 255, 0);
+        case 4: return color(255, 0, 0);
+        case 5: return color(0, 204, 0);
         case 6: return color(0, 204, 255);
-        case 7: return color(15,30,50);
+        case 7: return color(89, 0, 179);
     }
 }
